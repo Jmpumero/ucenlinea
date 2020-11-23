1d54287142be0b32b97e6447ada4e41c81942fd6
jmpumero <jmpumero@gmail.com>
2020-11-23 18:54:03 -0400

    gg

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -102,7 +102,9 @@ class UsersTableSeeder extends Seeder
         App\User::create([ //proveedor  8
             'name'      => 'PROVEEDOR Open E.',
             'email'     => 'pro@gmail.com',
+            'prioridad' => 'alta',
             'ci'  =>'V102',
+            'sexo' =>'hombre',
             'status' => true,
             'password'     => bcrypt('123'),
 
