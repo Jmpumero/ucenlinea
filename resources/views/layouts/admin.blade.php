691ac4f88f0de8f17a95e6694fdceb298cf2830d
jmpumero <jmpumero@gmail.com>
2021-02-03 15:08:44 -0400

    1.1.1

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -170,13 +170,7 @@
 
 
 
-            <!--interesante
-            <li class="nav-item">
-              <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#">
-                <i class="fas fa-th-large"></i>
-              </a>
-            </li>
-            -->
+           
           </ul>
         </nav>
         <!-- /.navbar -->
