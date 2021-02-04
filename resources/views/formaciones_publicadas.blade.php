99e43efbdce46eb0b601205e7fe4ec1f6f534292
jmpumero <jmpumero@gmail.com>
2021-02-03 21:15:54 -0400

    limpieza

--- resources/views/formaciones_publicadas.blade.php
+++ resources/views/formaciones_publicadas.blade.php
@@ -62,30 +62,7 @@
       <nav class="nav-menu d-none d-lg-block">
         <ul>
           <li><a href="{{ url('/') }}">Inicio</a></li>
-          <!--<li><a href="about.html">About</a></li>
-          <li class="active"><a href="courses.html">Courses</a></li>
-          <li><a href="trainers.html">Trainers</a></li>
-          <li><a href="events.html">Events</a></li>
-          <li><a href="pricing.html">Pricing</a></li>
-          <li class="drop-down"><a href="">Drop Down</a>
-            <ul>
-              <li><a href="#">Drop Down 1</a></li>
-              <li class="drop-down"><a href="#">Deep Drop Down</a>
-                <ul>
-                  <li><a href="#">Deep Drop Down 1</a></li>
-                  <li><a href="#">Deep Drop Down 2</a></li>
-                  <li><a href="#">Deep Drop Down 3</a></li>
-                  <li><a href="#">Deep Drop Down 4</a></li>
-                  <li><a href="#">Deep Drop Down 5</a></li>
-                </ul>
-              </li>
-              <li><a href="#">Drop Down 2</a></li>
-              <li><a href="#">Drop Down 3</a></li>
-              <li><a href="#">Drop Down 4</a></li>
-            </ul>
-          </li>
-          <li><a href="contact.html">Contact</a></li>
-        -->
+         
 
         </ul>
       </nav><!-- .nav-menu -->
